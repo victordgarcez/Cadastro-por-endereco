5a0015fd953a699caffa90a14679490414d9f8f1
Victor Garcez <contatovictorhdg@gmail.com>
2025-07-24 12:32:56 -0300

    Delete design-patterns directory

--- design-patterns/src/main/java/victordgarcez/design_patterns/model/EnderecoRepository.java
+++
@@ -1,9 +0,0 @@
-package victordgarcez.design_patterns.model;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository //strategy - design patterns
-public interface EnderecoRepository extends CrudRepository<Endereco, String> {
-
-}
